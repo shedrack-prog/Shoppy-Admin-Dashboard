2ed2212038365d230d9b7a890d70979843ac097c
shedrack-prog <usheddy07@gmail.com>
2023-06-21 18:15:52 +0100

    changed import statements

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts';
+import {
+  SparklineComponent,
+  Inject,
+  SparklineTooltip,
+} from '@syncfusion/ej2-react-charts';
 
-class SparkLine extends React.PureComponent {
+class SparkLineComp extends React.PureComponent {
   render() {
     const { id, height, width, color, data, type, currentColor } = this.props;
 
@@ -34,4 +38,4 @@ class SparkLine extends React.PureComponent {
   }
 }
 
-export default SparkLine;
+export default SparkLineComp;
